ab81e473deb4d94c01393ccce8146f22eb3ac6cd
Parveen Parmar <pparmar@wsgc.com>
2020-04-12 17:31:16 +0530

    recursion simple

---
+++ src/com/p3arl/algo/recursion/App.java
@@ -0,0 +1,15 @@
+package com.p3arl.algo.recursion;
+
+public class App {
+
+    public static void main(String[] args) {
+        reduceByOne(10);
+    }
+
+    public static void reduceByOne(int n) {
+        if(n >= 0)
+            reduceByOne(n-1);
+
+        System.out.println("Completed call: " + n);
+    }
+}
