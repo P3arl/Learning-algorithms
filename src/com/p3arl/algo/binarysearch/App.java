92a1146dcda5e9c4410b1fd378fb60e661f3f7f4
Parveen Parmar <pparmar@wsgc.com>
2020-04-12 17:03:52 +0530

    binary search

--- src/com/p3arl/algo/binarysearch/App.java
+++ src/com/p3arl/algo/binarysearch/App.java
@@ -1,4 +1,22 @@
 package com.p3arl.algo.binarysearch;
 
 public class App {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,7,12,18};
+        System.out.println(binarySearch(a, 20));
+    }
+
+    public static int binarySearch(int[] a, int x) {
+        int p = 0;
+        int r = a.length - 1;
+
+        while (p <= r) {
+            int q = (p + r)/2;
+
+            if(x < a[q]) r = q-1;
+            else if(x > a[q]) p = q+1;
+            else return q;
+        }
+        return -1;
+    }
 }
